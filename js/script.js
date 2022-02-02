a690d058bce2539cea97a9fb61f077f7386521c1
gfpaxa <gfpaxa@gmail.com>
2022-02-02 16:02:49 +0200

    02.02.22

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-let answer = confirm("Are you 18?");
-if (answer) {
-    console.log('Ok')
-} else {
-    console.log('Get out')
-}
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    dotsClass: 'my-dots'
+                }
+            }
+        ]
+      });
+  });
