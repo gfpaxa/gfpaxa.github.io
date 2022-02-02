d148b420b62ea7d5fba5c0606a3063ebf6b5a368
gfpaxa <gfpaxa@gmail.com>
2022-02-02 16:15:50 +0200

    02.02.22_wife_comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        speed: 1200,
+        speed: 600,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
